4021fd71e98512a5150122056a1e6cf11a1beffc
Robinson <agudelorobin31@gmail.com>
2023-08-25 11:47:52 -0500

    arreglos

--- Ciclos.py
+++ Ciclos.py
@@ -3,7 +3,7 @@ peluches = ["roshi","conejito","aguacate"]
 print (peluches)
 
 opcion=0
-print("Pelicheria ")
+print("Pelucheria ")
 print("...................")
 print("1. Agregar productos a la bodega ")
 print("2. Ver productos en bodega ")
@@ -14,13 +14,16 @@ print("5. SALIR")
 while(opcion!=5):
     opcion=int(input("Digite un numero: "))
 
-    if opcion==1:
-     print ("Usted esta en la opcion 1")
-    elif opcion==2:
-       print("Usted esta en la opcion 2")
-    elif opcion==3:
+    if (opcion==1):
+        nombre=input("Digita el nombre del producto: ")
+        #agregando datos a una lista
+        peluches.append(nombre)
+        print("peluche agregado... ")
+    elif (opcion==2):
+       print(peluches)
+    elif (opcion==3):
        print("Usted esta en la opcion 3")
-    elif opcion==4:
+    elif (opcion==4):
        print("Usted esta en la opcion 4")
     elif opcion==5:
        print("SALIR")
